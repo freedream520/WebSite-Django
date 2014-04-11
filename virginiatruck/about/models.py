bf80fb35f5cf1794e6105932a77ae3a62720ace5
Dennys Solis <dennyssolis@gmail.com>
2014-04-10 20:29:04 -0400

    page about creating

--- virginiatruck/about/models.py
+++ virginiatruck/about/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# creando modelos de datos para la aplicacion home
+# Creating data models for the application home
+
+class about(models.Model):
+	title = models.CharField(max_length=255)
+	content = models.TextField()
+	url_image = models.ImageField(upload_to='static/images/about')
+
+	def __unicode__(self):
+		return self.title
--- virginiatruck/about/views.py
+++ virginiatruck/about/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import about
 
 def ShowAbout(request):
-	abt = about.object.get(id:1)
+	abt = about.objects.get(id=1)
 	return render(request, 'about/about.html', {'abt':abt})
